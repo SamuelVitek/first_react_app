071c002d4f2b2aa9390f3651f8bc3f154b40b447
Samuel Vitek <Samuel.Vitek@aimtecglobal.com>
2023-05-08 23:20:03 +0200

    Adding first components

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,15 @@
+import './ExpenseDate.css'
+
+function ExpenseDate(props) {
+    const day = props.date.toLocaleString('en-US', { day: '2-digit'});
+    const month = props.date.toLocaleString('en-US', { month: 'long'});
+    const year = props.date.getFullYear();
+
+    return (
+        <div className="expense-date">
+            <div className="expense-date__month">{day}. {month} {year}</div>
+        </div>
+    )
+}
+
+export default ExpenseDate
