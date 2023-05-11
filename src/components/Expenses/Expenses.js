d01c132e3fa58378d15b16e51dbb4f8de3a93136
Samuel Vitek <Samuel.Vitek@aimtecglobal.com>
2023-05-11 02:04:01 +0200

    Updating list using form finished

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,10 @@
-import './Expenses.css'
+import React, {useState} from 'react';
+
+import ExpenseFilter from "./ExpenseFilter";
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
-import React, {useState} from "react";
-import ExpenseFilter from "./ExpenseFilter";
+
+import './Expenses.css'
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2023');
@@ -15,26 +17,13 @@ const Expenses = (props) => {
         <div>
             <Card className='expenses'>
                 <ExpenseFilter onFilterChange={onFilterChangeHandler} />
-                <ExpenseItem
-                    title={props.data[0].title}
-                    amount={props.data[0].amount}
-                    date={props.data[0].date}>
-                </ExpenseItem>
-                <ExpenseItem
-                    title={props.data[1].title}
-                    amount={props.data[1].amount}
-                    date={props.data[1].date}>
-                </ExpenseItem>
-                <ExpenseItem
-                    title={props.data[2].title}
-                    amount={props.data[2].amount}
-                    date={props.data[2].date}>
-                </ExpenseItem>
-                <ExpenseItem
-                    title={props.data[3].title}
-                    amount={props.data[3].amount}
-                    date={props.data[3].date}>
-                </ExpenseItem>
+                {props.items.map((expense) =>
+                    <ExpenseItem
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                )};
             </Card>
         </div>
     )
