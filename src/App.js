cf41de85a52eb5646d12e693367c403a14eaf742
Samuel Vitek <Samuel.Vitek@aimtecglobal.com>
2023-05-15 22:46:37 +0200

    Small bug fix

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ const DUMMY_EXPENSES = [
         id: 'e2',
         title: 'New TV',
         amount: 799.49,
-        date: new Date(2021, 2, 12)},
+        date: new Date(2021, 2, 12),
+    },
     {
         id: 'e3',
         title: 'Car Insurance',
